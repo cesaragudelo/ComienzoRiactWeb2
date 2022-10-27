455c480b21b69a5d87e0f0ab450cf0c953e1bbf6
205 <205@academicos.cesde.edu.co>
2022-10-26 20:43:17 -0500

    comienzo de react 26-10-2022

---
+++ src/Cesaragudelo/Cesaragudelo.js
@@ -0,0 +1,8 @@
+export function Cesaragudelo(){
+    return(
+        <> 
+        <h2>hola soy Cesar Agudelo</h2>
+        <h1>Ganando el curso</h1>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import{Componente1} from './Componente1/Componente1'
+import { Cesaragudelo } from './Cesaragudelo/Cesaragudelo.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <Componente1></Componente1>
+   <Cesaragudelo></Cesaragudelo>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
